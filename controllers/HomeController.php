3aa80fac0c43b3d2327e617ac7e11ee5b2069552
Mr.Z.Heng <1317201964@qq.com>
2017-06-13 00:03:24 +0800

    模板用法

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -4,14 +4,14 @@ use yii\base\Controller;
 
 class HomeController extends Controller{
 
+    public $layout="nimei";//设置默认父类模板文件
     public function actionIndex(){
-//        $arr = ['a'=>"AAA",'b'=>"BBB",'c'=>"CCC"];
-//        echo "<pre>";
-//        p($arr);
-//        echo "Hello World!!!";exit;
         $data = \Yii::$app->request;
         $order = $data->get("order_id",'999999999');
 //        dd($order);//exit;
-        return $this->renderPartial("index");
+        $user = array("name"=>"郑连新","age"=>"26","Country"=>"China");
+        $info = array("Company"=>"北京微指数科技有限公司","Location"=>"北京市朝阳区浦项中心","floor"=>"5");
+//        return $this->renderPartial("index",compact("user",'info'));
+        return $this->render("index",compact("user",'info'));
     }
 }
--- views/home/index.php
+++ views/home/index.php
@@ -5,4 +5,14 @@
  * Date: 2017/6/9
  * Time: 16:31
  */
-echo "我是index页面";
+?>
+<h1>个人信息</h1>
+<p>姓名：<?=$user['name']; ?></p>
+<p>年龄：<?=$user['age']; ?></p>
+<p>国籍：<?=$user['Country']; ?></p>
+<hr/>
+
+<h1>工作信息</h1>
+<p>公司：<?=$info['Company'];?></p>
+<p>位置：<?=$info['Location'];?></p>
+<p>楼层：<?=$info['floor'];?></p>
