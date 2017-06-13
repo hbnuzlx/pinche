b285751bf58fbda87fc4565babd6f42517ce67ed
zhenglianxin <1317201964@qq.com>
2017-06-13 10:02:32 +0800

    test

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -12,6 +12,11 @@ class HomeController extends Controller{
         $data = \Yii::$app->request;
         $order = $data->get("order_id",'999999999');
 //        dd($order);//exit;
-        return $this->renderPartial("index");
+        $user = array("name"=>"郑连新","age"=>"26");
+        $info = array("company"=>"北京微指数科技有限公司","location"=>"北京市朝阳区宏泰东街浦项中心B座5A");
+//        return $this->renderPartial("index",array("user"=>array("name"=>"郑连新","age"=>"26"),"info"=>array("company"=>"北京微指数科技有限公司","location"=>"北京市朝阳区宏泰东街浦项中心B座5A")));
+//        return $this->renderPartial("index",compact("user","info"));
+        return $this->render("index",compact("user","info"));
     }
+
 }
--- views/home/index.php
+++ views/home/index.php
@@ -5,4 +5,11 @@
  * Date: 2017/6/9
  * Time: 16:31
  */
-echo "我是index页面";
+echo "<h2>个人信息</h2>";
+echo '姓名：'.$user['name']."<br/>";
+echo '年龄：'.$user['age']."<br/>";
+
+echo "<h2>职业信息</h2>";
+echo "公司：".$info['company']."<br/>";
+echo "地址：".$info['location']."<br/>";
+echo \yii\helpers\Html::encode("<script>alert(123456)</script>");
